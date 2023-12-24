bae35f0893ee6da095128763c9fbdea54b515a21
Jürgen Leschner <jldec@users.noreply.github.com>
2023-12-24 00:44:37 +0000

    markdown tagline in Hero (#21)

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -2,7 +2,7 @@
   import { page } from '$app/stores';
   import type { Hero, Config } from '$lib/stores/model';
   import NavIcon from '$lib/components/NavIcon.svelte';
-  import { fixlink } from '$lib/fixlink';
+  import Markdoc from '$lib/components/Markdoc.svelte';
   import clsx from 'clsx';
 
   export let hero: Hero;
@@ -22,7 +22,7 @@
       {hero.text || pg?.subtitle || ''}
     </h2>
     <div class="text-xl my-4 text-slate-600 dark:text-slate-300">
-      {hero.tagline || pg?.description || ''}
+      <Markdoc doc={hero.tagline || pg?.description || ''} />
     </div>
     {#if hero.actionlinks}
       <div class="flex flex-row flex-wrap items-end gap-4 pb-4">
