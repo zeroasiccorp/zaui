c479250225a02cb6bc1d1f10f262c2c6b69acc05
jldec <jldec@users.noreply.github.com>
2023-12-31 01:11:31 +0000

    Add example

---
+++ example/src/app.config.ts
@@ -0,0 +1,15 @@
+import MyLayout from './components/MyLayout.svelte';
+
+const config: App.AppConfig = {
+  appComponents: {
+  },
+  layoutComponents: {
+    MyLayout,
+  },
+  markdownComponents: {
+  },
+  iconComponents: {
+  },
+};
+
+export default config;
